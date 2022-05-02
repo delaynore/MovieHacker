9451b7e9f8e245a0a2b9e5aaf88b2e1db3de3683
Daniil Lozhkin <delaynore@gmail.com>
2022-05-02 13:37:27 +0400

    added db model

---
+++ MovieHacker/Model/MHDataBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MovieHacker.Model
+{
+    public class MHDataBase : DbContext
+    {
+        public MHDataBase()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=mh.db");
+        }
+    }
+}
