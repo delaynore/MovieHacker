49a5d292abcf2eda22e53c31b979b275d21e1644
Daniil Lozhkin <delaynore@gmail.com>
2022-05-03 18:37:23 +0400

    db filler

---
+++ MovieHacker/Model/DefautValues/DataFiller.cs
@@ -0,0 +1,106 @@
+﻿using MovieHacker.Model.Tables;
+using System;
+using System.Collections.Generic;
+
+namespace MovieHacker.Model
+{
+    partial class DataFiller
+    {
+        public static void Fill()
+        {
+            using (MHDataBase db = new MHDataBase())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.SaveChanges();
+
+                var genres = new[]
+                {
+                    new Genre { GenreName = "Комедия" },
+                    new Genre { GenreName = "Мультфильм" },
+                    new Genre { GenreName = "Триллер" },
+                    new Genre { GenreName = "Ужасы" },
+                    new Genre { GenreName = "Фантастика" },
+                    new Genre { GenreName = "Боевик" },
+                    new Genre { GenreName = "Драма"  },
+                    new Genre { GenreName = "Документальный" },
+                    new Genre { GenreName = "Биография" },
+                    new Genre { GenreName = "Фэнтези" }
+                };
+                db.Genres.AddRange(genres);
+
+                var cinemas = new[]
+                {
+                    new Cinema { CinemaName = "Петровский" },
+                    new Cinema { CinemaName = "Матрица"},
+                    new Cinema { CinemaName = "Киномакс"},
+                    new Cinema { CinemaName = "Дядя Федор"},
+                    new Cinema { CinemaName = "Россия"}
+                };
+                var rand = new Random();
+                var filmrooms = new[] 
+                {
+                    new FilmRoom { NameRoom = "Красный", Capacity = rand.Next(20,50), Cinema = cinemas[0] },
+                    new FilmRoom { NameRoom = "Желтый", Capacity = rand.Next(20,50), Cinema = cinemas[0] },
+                    new FilmRoom { NameRoom = "Оранжевый", Capacity = rand.Next(20,50), Cinema = cinemas[0] },
+                    new FilmRoom { NameRoom = "Васильковый", Capacity = rand.Next(20,50), Cinema = cinemas[0] },
+
+                    new FilmRoom { NameRoom = "Первый", Capacity = rand.Next(30,60), Cinema = cinemas[1] },
+                    new FilmRoom { NameRoom = "Второй", Capacity = rand.Next(30,60), Cinema = cinemas[1] },
+                    new FilmRoom { NameRoom = "Третий", Capacity = rand.Next(30,60), Cinema = cinemas[1] },
+                    new FilmRoom { NameRoom = "Четвертый", Capacity = rand.Next(20,50), Cinema = cinemas[1] },
+                    new FilmRoom { NameRoom = "Пятый", Capacity = rand.Next(40,90), Cinema = cinemas[1] },
+                    new FilmRoom { NameRoom = "Шестой", Capacity = rand.Next(10,20), Cinema = cinemas[1] },
+
+                    new FilmRoom { NameRoom = "Кирпичный", Capacity = rand.Next(20,50), Cinema = cinemas[2] },
+                    new FilmRoom { NameRoom = "Бетонный", Capacity = rand.Next(20,50), Cinema = cinemas[2] },
+
+                    new FilmRoom { NameRoom = "Первый", Capacity = rand.Next(50,75), Cinema = cinemas[3] },
+                    new FilmRoom { NameRoom = "Второй", Capacity = rand.Next(50,75), Cinema = cinemas[3] },
+                    new FilmRoom { NameRoom = "Третий", Capacity = rand.Next(50,75), Cinema = cinemas[3] },
+                    new FilmRoom { NameRoom = "Четвертый", Capacity = rand.Next(50,75), Cinema = cinemas[3] },
+
+                    new FilmRoom { NameRoom = "Алмазный", Capacity = rand.Next(60,90), Cinema = cinemas[4] },
+                    new FilmRoom { NameRoom = "Рубиновый", Capacity = rand.Next(60,90), Cinema = cinemas[4] },
+                    new FilmRoom { NameRoom = "Аметистовый", Capacity = rand.Next(60,90), Cinema = cinemas[4] },
+                    new FilmRoom { NameRoom = "Рубиновый", Capacity = rand.Next(60,90), Cinema = cinemas[4] },
+                };
+
+                cinemas[0].FilmRooms = new List<FilmRoom>(filmrooms[0..4]);
+                cinemas[1].FilmRooms = new List<FilmRoom>(filmrooms[4..10]);
+                cinemas[2].FilmRooms = new List<FilmRoom>(filmrooms[10..12]);
+                cinemas[3].FilmRooms = new List<FilmRoom>(filmrooms[12..16]);
+                cinemas[4].FilmRooms = new List<FilmRoom>(filmrooms[16..]);
+                db.Cinemas.AddRange(cinemas);
+                db.FilmRooms.AddRange(filmrooms);
+
+                var movies = new[]
+                {
+                    new Movie { MovieName = "Люди в черном", DurationInMinutes = rand.Next(100, 240), Genre = genres[4]},
+                    new Movie { MovieName = "Мстители", DurationInMinutes = rand.Next(100, 240), Genre = genres[4]},
+                    new Movie { MovieName = "Человек-паук 3", DurationInMinutes = rand.Next(100, 240), Genre = genres[4]},
+                    new Movie { MovieName = "Титаник", DurationInMinutes = rand.Next(100, 240), Genre = genres[6]},
+                    new Movie { MovieName = "Маска", DurationInMinutes = rand.Next(100, 240), Genre = genres[0]},
+                    new Movie { MovieName = "Волшебник страны Оз", DurationInMinutes = rand.Next(100, 240), Genre = genres[9]},
+                };
+                db.Movies.AddRange(movies);
+
+                List<Session> sessions = new List<Session>();
+                for (int i = 0; i < 100; i++)
+                {
+                    var num = rand.Next(20);
+                    sessions.Add(new Session
+                    {
+                        Movie = movies[rand.Next(movies.Length)],
+                        FilmRoom = filmrooms[num],
+                        NumberAvailableSeats = filmrooms[num].Capacity,
+                        Price = 320,
+                        StartTime = new DateTime(2022, rand.Next(1,13), rand.Next(1,20))
+                    });
+                }
+                db.Sessions.AddRange(sessions);
+                db.SaveChanges();
+            }
+        }
+    }
+}
--- MovieHacker/Model/MHDataBase.cs
+++ MovieHacker/Model/MHDataBase.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MovieHacker.Model.Tables;
+using System.Collections.Generic;
 
 namespace MovieHacker.Model
 {
@@ -20,5 +21,32 @@ namespace MovieHacker.Model
         {
             optionsBuilder.UseSqlite("Data Source=mh.db");
         }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Genre>().HasData(new[]
+        //    {
+        //        new Genre { Id = 1, Name = "Комедия" },
+        //        new Genre { Id = 2, Name = "Мультфильм" },
+        //        new Genre { Id = 3, Name = "Триллер" },
+        //        new Genre { Id = 4, Name = "Ужасы" },
+        //        new Genre { Id = 5, Name = "Фантастика" },
+        //        new Genre { Id = 6, Name = "Боевик" },
+        //        new Genre { Id = 7, Name = "Драма"  },
+        //        new Genre { Id = 8, Name = "Документальный" },
+        //        new Genre { Id = 9, Name = "Биография" },
+        //        new Genre { Id = 10, Name = "Фэнтези" }
+        //    });
+
+        //    modelBuilder.Entity<Cinema>().HasData(new[]
+        //    {
+        //        new Cinema { Id = 1, Name = "Петровский" },
+        //        new Cinema { Id = 2, Name = "Матрица"},
+        //        new Cinema { Id = 3, Name = "Киномакс"},
+        //        new Cinema { Id = 4, Name = "Дядя Федор"},
+        //        new Cinema { Id = 5, Name = "Россия"}
+        //    });
+
+        //}
     }
 }
