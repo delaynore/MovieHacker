ee8e3f754aae90c382853eed3add758360ad99f6
Daniil Lozhkin <delaynore@gmail.com>
2022-05-27 23:16:54 +0400

    added for genre and movie controller

---
+++ MovieHacker/Model/GenreController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using MovieHacker.Model.Tables;
+
+namespace MovieHacker.Model
+{
+    public class GenreController : IDataBaseController<Genre>
+    {
+        public void Add(Genre x)
+        {
+            using(var db = new MHDataBase())
+            {
+                db.Genres.Add(x);
+                db.SaveChanges();
+            }
+        }
+
+        public void AddRange(params Genre[] x)
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Genres.AddRange(x);
+                db.SaveChanges();
+            }
+        }
+
+        public Genre? Get(int id)
+        {
+            using (var db = new MHDataBase())
+            {
+                var res = db.Genres.FirstOrDefault(x => x.Id == id);
+                return res;
+            }
+        }
+
+        public List<Genre> GetAll()
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Genres.Load();
+                return db.Genres.ToList();
+            }
+        }
+
+        public void Remove(Genre x)
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Genres.Remove(x);
+                db.SaveChanges();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            using (var db = new MHDataBase())
+            {
+                try
+                {
+                    db.Genres.Remove(db.Genres.First(x => x.Id == index));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ MovieHacker/Model/MovieController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using MovieHacker.Model.Tables;
+
+namespace MovieHacker.Model
+{
+    public class MovieController : IDataBaseController<Movie>
+    {
+        public void Add(Movie x)
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Movies.Add(x);
+                db.SaveChanges();
+            }
+        }
+
+        public void AddRange(params Movie[] x)
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Movies.AddRange(x);
+                db.SaveChanges();
+            }
+        }
+
+        public Movie? Get(int id)
+        {
+            using (var db = new MHDataBase())
+            {
+                var res = db.Movies.FirstOrDefault(x => x.Id == id);
+                return res;
+            }
+        }
+
+        public List<Movie> GetAll()
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Movies.Load();
+                return db.Movies.ToList();
+            }
+        }
+
+        public void Remove(Movie x)
+        {
+            using (var db = new MHDataBase())
+            {
+                db.Movies.Remove(x);
+                db.SaveChanges();
+            }
+        }
+
+        public void RemoveAt(int index)
+        {
+            using (var db = new MHDataBase())
+            {
+                try
+                {
+                    db.Genres.Remove(db.Genres.First(x => x.Id == index));
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                finally
+                {
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
