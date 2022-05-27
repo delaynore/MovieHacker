c66ae489302748cff0fb21d03660126efacd4922
Daniil Lozhkin <delaynore@gmail.com>
2022-05-27 20:10:18 +0400

    added interface for db conrol

---
+++ MovieHacker/Model/IDataBaseController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieHacker.Model
+{
+    interface IDataBaseController<T> 
+        where T: class
+    {
+        public void Add(T x);
+        public void AddRange(params T[] x);
+        public void Remove(T x);
+        public void RemoveAt(int index);
+        public T? Get(int id);
+        public List<T> GetAll();
+
+    }
+}
