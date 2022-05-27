2f7cba285686fdc38a2769ac3aa08dda552377a1
Daniil Lozhkin <delaynore@gmail.com>
2022-05-27 20:10:59 +0400

    add private type for updating data

---
+++ MovieHacker/Model/BoolEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieHacker.Model
+{
+    public class BoolEvent
+    {
+        public BoolEvent()
+        {
+            Variable = false;
+        }
+        public event Action? ForDo;
+        private bool variable;
+
+        public bool Variable
+        {
+            get { return variable; }
+            set 
+            {
+                variable = value;
+                if(variable)
+                    ForDo?.Invoke();
+            }
+        }
+    }
+}
