2f5a73ac46350ef1253e925352ca0ae435764f70
Daniil Lozhkin <delaynore@gmail.com>
2022-05-28 19:39:49 +0400

    movies page fix

--- MovieHacker/Views/PageMovies.xaml.cs
+++ MovieHacker/Views/PageMovies.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-
+using Microsoft.EntityFrameworkCore;
 using MovieHacker.Model;
+using MovieHacker.Model.Extensions;
 
 namespace MovieHacker.Views
 {
@@ -22,19 +23,22 @@ namespace MovieHacker.Views
         {
             using (var db = new MHDataBase())
             {
-                //var films = db.Movies.Join(db.Genres, m => m.Genres, g => g., (m, g) => new
-                //{
-                //    Id = m.Id,
-                //    Image = @"C:\Users\delay\source\repos\MovieHacker\MovieHacker\Resources\main_icon.ico",
-                //    MovieName = m.Title,
-                //    Genre = g.Name,
-                //    DurationInMinutes = m.DurationInMinutes
-                //}).ToArray();
-                //listBox1.ItemsSource = films;
-                //db.
-               //var fi = from p in db.Movies
-               //         join g in db.Genres on p.Genres.
+                var s = from p in db.Movies
+                        select new
+                        {
+                            Id = p.Id,
+                            MovieName = p.Title,
+                            Image = ImageBase64Converter.ToXAMLView(p.Picture.Path),
+                            Genre = string.Join(", ", p.Genres.Select(x=>x.Genre.Name).ToList()),
+                            DurationInMinutes = p.DurationInMinutes
+                        };
+                listBox1.ItemsSource = s.ToArray();
             }
+        }
+
+        private void addMovie_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
