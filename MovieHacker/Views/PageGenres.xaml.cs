d62d015196b7442d38ab8f457c8bb5fc3e33a1a8
Daniil Lozhkin <delaynore@gmail.com>
2022-05-31 14:02:25 +0400

    page genres completed (mb)

--- MovieHacker/Views/PageGenres.xaml.cs
+++ MovieHacker/Views/PageGenres.xaml.cs
@@ -25,7 +25,8 @@ namespace MovieHacker.Views
             using (var db = new MHDataBase())
             {
                 db.Genres.Load();
-                listBoGenres.ItemsSource = db.Genres.Local.Select(x => x.Name).ToList();
+                listBoGenres.ItemsSource = db.Genres.Local.Select(x => x.Name).Where(x => x.Contains(textBoxFind.Text)).ToList();
+                checker.Variable = false;
             }
         }
         private void Page_Loaded(object sender, RoutedEventArgs e)
@@ -36,6 +37,7 @@ namespace MovieHacker.Views
         private void addGenre_Click(object sender, RoutedEventArgs e)
         {
             new CreateAndEditGenreWindow(new AddNewGenreMode()).ShowDialog();
+            checker.Variable = true;
         }
 
         private void editGenre_Click(object sender, RoutedEventArgs e)
@@ -43,6 +45,29 @@ namespace MovieHacker.Views
             var s = new MHDataBase().Genres.Where(x=>x.Name == listBoGenres.SelectedItem.ToString()).First();
             if (s == null) return;
             new CreateAndEditGenreWindow(new EditGenreMode(s.Id, s.Name)).ShowDialog();
+            checker.Variable = true;
+        }
+
+        private void deleteGenre_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new MHDataBase().Genres.Where(x => x.Name == listBoGenres.SelectedItem.ToString()).First();
+            if (s == null) return;
+            if (MessageBox.Show($"Вы действительное хотите удалить жанр - {s.Name}", "Подтверждение", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                new GenreController().RemoveAt(s.Id);
+                checker.Variable = true;
+            }
+                
+        }
+
+        private void updateGenre_Click(object sender, RoutedEventArgs e)
+        {
+            checker.Variable = true;
+        }
+
+        private void TextBoxFind_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            checker.Variable = true;
         }
     }
 }
